643a45c6e3293e1808d51341e1b2c4f4acaeeb67
thenasos <thenasos@gmail.com>
2018-03-22 00:19:51 +0200

    Content refreshes automatically

--- style/tourney.js
+++ style/tourney.js
@@ -8,7 +8,13 @@ function startTime() {
     s = checkTime(s);
     document.getElementById('txt').innerHTML =
     h + ":" + m + ":" + s;
+  	$( "#game1" ).load( "names.html .game1");
+	$( "#game2" ).load( "names.html .game2");
+	$( ".next1" ).load( "names.html #next1 li");
+	$( ".next2" ).load( "names.html #next2 li");
     var t = setTimeout(startTime, 500);
+	
+	
 }
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
