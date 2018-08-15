3c4914ef262c4a824b87c6828510cf8f59e4f99d
thenasos <thenasos@gmail.com>
2018-08-15 22:01:56 +0300

    Names displayed of current fights

--- style/tourney.js
+++ style/tourney.js
@@ -1,6 +1,13 @@
 var totalNodes = "";
 var matchName = "";
 var currentFight1 = "";
+var currentFight2
+var apik= "";
+var participantsList = "";
+var currentFight1disp = "";
+var currentFight2disp = "";
+
+
 $.ajax({
 type     : "GET",
 url      : "tournaments.xml",
@@ -17,12 +24,27 @@ url      : "matches.xml",
 dataType : "xml",
 success  : function(xmlData){
 	totalNodes = $('match',xmlData);
-	//currentFight1 = $('player1-id',xmlData)
 	for (var i=0; i<totalNodes.length; i++){
 		if ($('state',xmlData)[i].innerHTML === "open"){
-			var currentFight1 = $('player1-id',xmlData)[i].innerHTML;
-			var currentFight2 = $('player2-id',xmlData)[i].innerHTML;
-			$('#next1')[0].innerHTML = currentFight1 + " vs " + currentFight2;
-			//alert(currentFight1 + " vs " + currentFight2);
+			currentFight1 = $('player1-id',xmlData)[i].innerHTML;
+			currentFight2 = $('player2-id',xmlData)[i].innerHTML;
+			$.ajax({
+			type     : "GET",
+			url      : "participants.xml",
+			dataType : "xml",
+			success  : function(xmlData){
+				participantsList = $('id', xmlData);
+				for (var x=0; x<participantsList.length; x++){
+					if ($('id', xmlData)[x].innerHTML === currentFight1){
+						currentFight1disp = $('name', xmlData)[x].innerHTML;
+					}
+					if ($('id', xmlData)[x].innerHTML === currentFight2){
+						currentFight2disp = $('name', xmlData)[x].innerHTML;
+					}
+					$('.fighting')[0].innerHTML = currentFight1disp + " vs " + currentFight2disp;
+				}
+			
+			}});
+
 	}}
 }});
